1cd9356c9b4f2208a88d1c34164a6bfc71584347
hee462 <a01080809887@gmail.com>
2024-04-06 16:25:58 +0900

    feat : save() ,load() 구현 (initSate)

--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -16,10 +16,17 @@ class _MainScreenState extends State<MainScreen> {
   final _heightController = TextEditingController();
   final _weightController = TextEditingController();
 
+  // 화면 처음 시작하는 부분에 선언
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    load();
+  }
+
 // 메모리 리셋
   @override
   void dispose() {
-    save();
     _heightController.dispose();
     _weightController.dispose();
     super.dispose();
@@ -29,6 +36,19 @@ class _MainScreenState extends State<MainScreen> {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setDouble('height', double.parse(_heightController.text));
     await prefs.setDouble('weight', double.parse(_weightController.text));
+  }
+
+  // 왜 nullable type으로 선언하는가?
+  // 읿력 받은 값이 없으면 동작되지 않아서 터지기 때문에
+  Future load() async {
+    final prefs = await SharedPreferences.getInstance();
+    final double? height = prefs.getDouble('height');
+    final double? weight = prefs.getDouble('weight');
+    // 입력값을 있는지 없는지 확인하고 키값에 담아
+    if (height != null && weight != null) {
+      _heightController.text = '$height';
+      _weightController.text = '$weight';
+    }
   }
 
   @override
@@ -82,7 +102,9 @@ class _MainScreenState extends State<MainScreen> {
                     if (_fromKey.currentState?.validate() == false) {
                       return;
                     }
-
+                    // form에 입력한 값 저장하기
+                    save();
+                    // 저장 받은 값을 다음 페이지에 값 넘겨줌
                     Navigator.push(
                       context,
                       MaterialPageRoute(
